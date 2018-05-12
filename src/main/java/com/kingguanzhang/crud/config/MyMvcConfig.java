740be571b8a3539363af452ed159fb9b9a6a0706
EmptyWalletException <1833865798@qq.com>
2018-05-12 12:10:44 +0800

    首次测试idea与Git和GitHub整合

---
+++ src/main/java/com/kingguanzhang/crud/config/MyMvcConfig.java
@@ -0,0 +1,36 @@
+package com.kingguanzhang.crud.config;
+
+import com.kingguanzhang.crud.component.MyLocaleResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.Locale;
+
+/**
+ * 扩展springMvc
+ */
+@Configuration
+public class MyMvcConfig extends WebMvcConfigurerAdapter {
+    @Override
+    /*注册视图解析器,所有在localhost:8080后面加上的路径会被springMvc以返回的字符串解析
+    并在templates路径下寻找对应的html*/
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/test").setViewName("success");
+        registry.addViewController("/test.html").setViewName("success");
+        registry.addViewController("/").setViewName("login");
+        registry.addViewController("/index").setViewName("login");
+        registry.addViewController("/index.html").setViewName("login");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/login.html").setViewName("login");
+        registry.addViewController("/board").setViewName("dashboard");
+        registry.addViewController("/board.html").setViewName("dashboard");
+    }
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new MyLocaleResolver();
+    }
+}
